9a62761efdf6d813d474de2e723e9461443c6a01
Felicia Salim <feliciasalim08@gmail.com>
2025-06-12 16:27:44 +0700

    Upload project files

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: './', // Set the root directory
+  server: {
+    port: 3000, // Specify the development server port
+  },
+});
